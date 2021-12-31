076e59aea699e045d2103377b6eaf883b2f975ca
Bailey Kane <bailey.orion.kane@gmail.com>
2021-12-31 15:23:44 +0800

    Formatting fixes

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,4 @@
-import Head from "next/head"
 import { Component } from 'react'
-import { attributes, react as HomeContent } from '../content/home.md';
 
 export default class About extends Component {
   render() {
