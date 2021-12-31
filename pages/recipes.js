85b1ccbf067f5f0d2bdcfc5a063faad353612bb8
Bailey Kane <bailey.orion.kane@gmail.com>
2021-12-31 16:35:10 +0800

    Reformat recipes.js to follow export default class... format

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,17 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
+import {
+  attributes,
+  react as RecipesContent,
+} from "../content/recipes/my-new-recipe.md";
 
-function Recipes({ recipes }) {
-  return (
-    <ul>
-      {/* {recipes.map((recipe) => ( */}
+export default class Recipes extends Component {
+  render() {
+    let { title, date, thumbnail } = attributes;
+    return (
+      <ul>
+        {/* {recipes.map((recipe) => ( */}
         {/* <li key={recipe.id}> */}
-          {/* <Link href={`/recipes/${encodeURIComponent(recipe.slug)}`}> */}
-            {/* <a>{recipe.title}</a> */}
-          {/* </Link> */}
+        {/* <Link href={`/recipes/${encodeURIComponent(recipe.slug)}`}> */}
+        {/* <a>{recipe.title}</a> */}
+        {/* </Link> */}
         {/* </li> */}
-      {/* ))} */}
-    </ul>
-  )
+        {/* ))} */}
+      </ul>
+    );
+  }
 }
-
-export default Recipes
