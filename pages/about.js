d942dada09e3f380ea7256c7da8b890272a66bae
Bailey Kane <bailey.orion.kane@gmail.com>
2021-12-31 15:18:06 +0800

    Created about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import { Component } from 'react'
+import { attributes, react as HomeContent } from '../content/home.md';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        About!
+      </div>
+    )
+  }
+}
