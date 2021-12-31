c8defff1f79d082845db9c5276fd38b896064f37
Bailey Kane <bailey.orion.kane@gmail.com>
2021-12-31 16:13:16 +0800

    Temporarily comment out recipes page content to test admin config setup

--- pages/recipes.js
+++ pages/recipes.js
@@ -3,13 +3,13 @@ import Link from 'next/link'
 function Recipes({ recipes }) {
   return (
     <ul>
-      {recipes.map((recipe) => (
-        <li key={recipe.id}>
+      {/* {recipes.map((recipe) => ( */}
+        {/* <li key={recipe.id}> */}
           {/* <Link href={`/recipes/${encodeURIComponent(recipe.slug)}`}> */}
-            <a>{recipe.title}</a>
+            {/* <a>{recipe.title}</a> */}
           {/* </Link> */}
-        </li>
-      ))}
+        {/* </li> */}
+      {/* ))} */}
     </ul>
   )
 }
