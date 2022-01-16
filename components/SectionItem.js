e32e088f0f84b58e5e0bd48a31bf72b4001f53ae
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-16 23:05:58 +0800

    Cleaned up formatting. Removed accidentally redundant classes

--- components/SectionItem.js
+++ components/SectionItem.js
@@ -4,8 +4,14 @@ import { Component } from "react";
 export default class SectionItem extends Component {
   render() {
     return (
-      <div // Should make this into a separate component... Can be reused everywhere. And, less of a pain to see all the CSS
-        className="group p-2 border-solid border-2 rounded-lg even:rotate-1 odd:-rotate-1 border-violet-200 bg-gradient-to-r from-violet-100 to-orange-100 dark:from-violet-600 dark:to-orange-400 shadow-md shadow-violet-300 dark:shadow-violet-800 transition hover:from-violet-200 hover:to-orange-200 hover:shadow-lg hover:shadow-violet-300 hover:dark:from-violet-700 hover:dark:to-orange-500 hover:dark:shadow-violet-700 hover:scale-105 active:border-violet-300 dark:active:border-violet-700 active:bg-violet-200 dark:active:bg-violet-700"
+      <div
+        className="group p-2 border-solid border-2 rounded-lg
+                   even:rotate-1 odd:-rotate-1
+                   bg-gradient-to-r from-violet-100 to-orange-100 dark:from-violet-600 dark:to-orange-400
+                   hover:from-violet-200 hover:to-orange-200 hover:dark:from-violet-700 hover:dark:to-orange-500
+                   border-violet-200 active:border-violet-300 dark:active:border-violet-400
+                   shadow-md hover:shadow-lg shadow-violet-300 hover:shadow-violet-300 dark:shadow-violet-800 hover:dark:shadow-violet-700
+                   transition hover:scale-105"
       >
         <Link href={"/" + this.props.name.toLowerCase()}>
           <a className="grid grid-cols-2">
