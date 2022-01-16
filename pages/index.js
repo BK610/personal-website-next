61388818fa400771d1f95d631596f4a32e0ef282
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-16 22:23:24 +0800

    Added spinning to the icons

--- pages/index.js
+++ pages/index.js
@@ -20,18 +20,25 @@ export default class Home extends Component {
           </div>
           {sections.map((section, k) => (
             <div // Should make this into a separate component... Can be reused everywhere. And, less of a pain to see all the CSS
-              className="p-2 border-solid border-2 rounded-lg even:rotate-1 odd:-rotate-1 border-violet-200 bg-gradient-to-r from-violet-100 to-orange-100 dark:from-violet-600 dark:to-orange-400 shadow-md shadow-violet-300 dark:shadow-violet-800 transition hover:from-violet-200 hover:to-orange-200 hover:shadow-lg hover:shadow-violet-300 hover:dark:from-violet-700 hover:dark:to-orange-500 hover:dark:shadow-violet-700 hover:scale-105 active:border-violet-700 active:bg-violet-200 dark:active:bg-violet-700"
+              className="group p-2 border-solid border-2 rounded-lg even:rotate-1 odd:-rotate-1 border-violet-200 bg-gradient-to-r from-violet-100 to-orange-100 dark:from-violet-600 dark:to-orange-400 shadow-md shadow-violet-300 dark:shadow-violet-800 transition hover:from-violet-200 hover:to-orange-200 hover:shadow-lg hover:shadow-violet-300 hover:dark:from-violet-700 hover:dark:to-orange-500 hover:dark:shadow-violet-700 hover:scale-105 active:border-violet-300 dark:active:border-violet-700 active:bg-violet-200 dark:active:bg-violet-700"
               key={k}
             >
               <Link href={"/" + section.name.toLowerCase()}>
-                <a>
-                  <h2 className="font-medium dark:text-violet-100">
-                    {section.name}
-                  </h2>
-                  <p className="font-light text-sm text-zinc-700 dark:text-zinc-200">
-                    {section.description}
-                  </p>
-                  <img className="dark:invert h-8" src={section.icon}></img>
+                <a className="grid grid-cols-2">
+                  <div>
+                    <h2 className="font-medium dark:text-violet-100">
+                      {section.name}
+                    </h2>
+                    <p className="font-light text-sm text-zinc-700 dark:text-zinc-200">
+                      {section.description}
+                    </p>
+                  </div>
+                  <div className="justify-self-end self-center">
+                    <img
+                      className="h-8 dark:invert group-hover:animate-spin"
+                      src={section.icon}
+                    ></img>
+                  </div>
                 </a>
               </Link>
             </div>
