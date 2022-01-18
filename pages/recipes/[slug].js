f086e7d4e06522bebf6f99991d617d29d95f9485
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-18 20:34:02 +0800

    Import info for recipe generic page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import MissingContent from "../../components/MissingContent";
 
 export default class Post extends Component {
-    static async getInitialProps({ query }) {
-      const { slug } = query;
-      const recipe = await import(`../../content/recipes/${slug}.md`).catch(error => null);
-  
-      return { recipe };
-    }
-    render() {
-      if (!this.props.recipe) return <div>not found</div>;
-  
-      const {
-        html,
-        attributes: { thumbnail, title },
-      } = this.props.recipe.default;
-  
-      return (
-        <>
-          <article>
-            <h1>{title}</h1>
-            <img src={thumbnail} />
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-          </article>
-          <style jsx>{`
-            article {
-              margin: 0 auto;
-            }
-            h1 {
-              text-align: center;
-            }
-          `}</style>
-        </>
-      );
-    }
+  static async getInitialProps({ query }) {
+    const { slug } = query;
+    const recipe = await import(`../../content/recipes/${slug}.md`).catch(
+      (error) => null
+    );
+
+    return { recipe };
   }
+
+  render() {
+    if (!this.props.recipe) return <MissingContent />;
+
+    console.log(this.props.recipe);
+    return (
+      <div>
+        Hello?
+        <h1>{this.props.recipe.attributes.title}</h1>
+        <img src={`../${this.props.recipe.attributes.thumbnail}`} /> 
+        <div>{this.props.recipe.attributes.body}</div>
+      </div>
+    );
+  }
+}
