adc1f2417cac0906a2f0ca8c4634aa37f8849bb1
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-18 20:20:45 +0800

    Created component for indicating missing info in case an incorrect URL slug is specified

---
+++ components/MissingContent.js
@@ -0,0 +1,7 @@
+import { Component } from "react";
+
+export default class MissingContent extends Component {
+  render() {
+    return <div>The content you're looking for seems to be missing, friend 😕</div>;
+  }
+}
