79d49b6424bdd3d13adcfc34f280255e432585d4
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-30 12:16:37 +0800

    Added custom animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,34 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "25%": { transform: "rotate(-1deg)" },
+          "75%": { transform: "rotate(1deg)" },
+        },
+        bounceleft: {
+          "0%, 100%": {
+            transform: "translateX(0%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateX(-25%)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
+      animation: {
+        spinslow: "spin 2s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+        bounceleft: "bounceleft 1s infinite",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ],
-}
+  variants: {
+    extend: {
+      animation: ["hover", "group-hover"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
