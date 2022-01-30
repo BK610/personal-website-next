210fcba9fa5ee335cf874d15be73616084a3a9eb
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-30 12:18:23 +0800

    Added custom hover animations and updated formatting

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,21 +7,32 @@ export default class NavBar extends Component {
       <div className="py-2 border-b border-zinc-500 dark:border-zinc-500">
         <Link href="/">
           <div className="w-fit mr-1 inline-block group transition hover:scale-105">
-            <a
-              className="p-1 font-light text-sm text-zinc-700 dark:text-zinc-300 cursor-pointer
+            <div className="group-hover:animate-wiggle">
+              <a
+                className="p-1 font-light text-sm text-zinc-700 dark:text-zinc-300 cursor-pointer
           border border-solid border-transparent rounded-lg hover:border-zinc-700 hover:dark:border-zinc-300"
-            >
-              <span className="inline-block group-hover:animate-bounce">←</span> Home
-            </a>
+              >
+                <span className="inline-block group-hover:animate-bounceleft">
+                  ←
+                </span>{" "}
+                Home
+              </a>
+            </div>
           </div>
         </Link>
         <div className="w-fit mr-1 inline-block group transition hover:scale-105">
-          <button onClick={() => history.back()}>
+          <button
+            className="group-hover:animate-wiggle"
+            onClick={() => history.back()}
+          >
             <a
               className="p-1 font-light text-sm text-zinc-700 dark:text-zinc-300 cursor-pointer
           border border-solid border-transparent rounded-lg hover:border-zinc-700 hover:dark:border-zinc-300"
             >
-              <span className="inline-block group-hover:animate-bounce">←</span> Back
+              <span className="inline-block group-hover:animate-bounceleft">
+                ←
+              </span>{" "}
+              Back
             </a>
           </button>
         </div>
