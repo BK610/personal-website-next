cc961229c8369c9c7d7f7ac5fcd7cbce7b9751b0
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-30 13:22:44 +0800

    Added bounceup animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,22 @@ module.exports = {
             "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
           },
         },
+        bounceup: {
+          "0%, 100%": {
+            transform: "translateY(0%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(-25%)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
       },
       animation: {
         spinslow: "spin 2s linear infinite",
         wiggle: "wiggle 1s ease-in-out infinite",
         bounceleft: "bounceleft 1s infinite",
+        bounceup: "bounceup 1s infinite",
       },
     },
   },
