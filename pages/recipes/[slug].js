2d497c95b90aac1499e02a125491725dd124d7f1
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-30 15:47:03 +0800

    Upgraded formatting for all content.

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -28,25 +28,58 @@ export default class Post extends Component {
       content,
     } = this.props.recipe.attributes;
 
-    console.log(ingredients);
+    var dateObj = new Date(date);
 
     return (
-      <div>
-        <NavBar/>
-        <div className="prose mt-4">
-          <h2>{title}</h2>
-          <div>{date}</div>
-          <div>{description}</div>
-          <div>{author}</div>
-          <div>{prepTime}</div>
-          <div>{totalTime}</div>
-          <div
-            dangerouslySetInnerHTML={{ __html: micromark(ingredients) }}
-          />
-          <img src={`../${thumbnail}`} className="h-64" />
-          <div
-            dangerouslySetInnerHTML={{ __html: micromark(content) }}
-          />
+      <div className="flex flex-col h-full min-h-screen w-full min-w-screen bg-gradient-to-b from-zinc-100 to-zinc-200 dark:from-zinc-800 dark:to-zinc-900 dark:text-gray-100 items-center">
+        <div className="p-2 pb-4 space-y-4">
+          <NavBar />
+          <div className="prose dark:prose-invert mt-4">
+            <h2>{title}</h2>
+            <div className="border-b border-b-zinc-500">
+              <span className="no-underline">{description}</span>
+            </div>
+            <div className="grid grid-cols-2">
+              <div className="col-span-2 sm:col-span-1 place-self-center">
+                <img
+                  className="rounded-lg object-contain max-h-72"
+                  src={`../${thumbnail}`}
+                />
+              </div>
+              <div className="col-span-2 sm:col-span-1 grid grid-cols-2 sm:grid-cols-3 space-x-2 self-center">
+                <div className="justify-self-end text-right underline decoration-1">
+                  <div>Author</div>
+                  <div>Prep Time</div>
+                  <div>Total Time</div>
+                  <div>Published</div>
+                </div>
+                <div className="col-span-1 sm:col-span-2 justify-end">
+                  <div>{author}</div>
+                  <div>{prepTime} min</div>
+                  <div>{totalTime} min</div>
+                  <div>
+                    {dateObj.toLocaleDateString("en-US", {
+                      weekday: "long",
+                      month: "short",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className="inline underline decoration-1">Ingredients</p>{" "}
+              <div
+                className="select-all"
+                dangerouslySetInnerHTML={{ __html: micromark(ingredients) }}
+              />
+            </div>
+            <div>
+              <p className="inline underline decoration-1">Recipe</p>{" "}
+              <div dangerouslySetInnerHTML={{ __html: micromark(content) }} />
+            </div>
+          </div>
         </div>
       </div>
     );
