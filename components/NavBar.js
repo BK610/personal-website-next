c296d221db6c32c848c3f19dfcfe6d72e4896c26
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-30 11:30:41 +0800

    Add back button to navbar. Restructure to inline display

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,16 +4,28 @@ import Link from "next/link";
 export default class NavBar extends Component {
   render() {
     return (
-      <Link href="/">
-        <div className="w-fit transition hover:scale-105">
-          <a
-            className="p-1 font-light text-sm text-zinc-700 dark:text-zinc-300 cursor-pointer
+      <div className="py-2 border-b border-zinc-500 dark:border-zinc-500">
+        <Link href="/">
+          <div className="w-fit mr-1 inline-block group transition hover:scale-105">
+            <a
+              className="p-1 font-light text-sm text-zinc-700 dark:text-zinc-300 cursor-pointer
           border border-solid border-transparent rounded-lg hover:border-zinc-700 hover:dark:border-zinc-300"
-          >
-            ← Home
-          </a>
+            >
+              <span className="inline-block group-hover:animate-bounce">←</span> Home
+            </a>
+          </div>
+        </Link>
+        <div className="w-fit mr-1 inline-block group transition hover:scale-105">
+          <button onClick={() => history.back()}>
+            <a
+              className="p-1 font-light text-sm text-zinc-700 dark:text-zinc-300 cursor-pointer
+          border border-solid border-transparent rounded-lg hover:border-zinc-700 hover:dark:border-zinc-300"
+            >
+              <span className="inline-block group-hover:animate-bounce">←</span> Back
+            </a>
+          </button>
         </div>
-      </Link>
+      </div>
     );
   }
 }
