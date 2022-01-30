7567665d1ed91a68331592c2ab8d1851d7518ffb
Bailey Kane <bailey.orion.kane@gmail.com>
2022-01-30 17:23:10 +0800

    Add Recipes Header, update formatting to be consistent with other pages

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -11,19 +11,26 @@ export default class Recipes extends Component {
   render() {
     const { recipesList } = this.props;
     return (
-      <div className="flex flex-col h-full min-h-screen w-full min-w-screen bg-gradient-to-b from-zinc-100 to-zinc-200 dark:from-zinc-800 dark:to-zinc-900 dark:text-gray-100 items-center">
-        <div className="w-full max-w-md p-2 pb-4 space-y-4 select-none">
-        <NavBar/>
-          {recipesList.map((recipe, k) => (
-            <SectionItem
-              link={"/recipes/" + recipe.slug.toLowerCase()}
-              name={recipe.attributes.title}
-              description={recipe.attributes.description}
-              icon={recipe.attributes.thumbnail}
-              key={k}
-              className={""}
-            />
-          ))}
+      <div className="flex flex-col h-full min-h-screen w-full min-w-screen bg-gradient-to-b from-zinc-100 to-zinc-200 dark:from-zinc-800 dark:to-zinc-900 dark:text-gray-100 items-center select-none">
+        <div className="max-w-2xl w-full p-2 pb-4 space-y-4">
+          <NavBar />
+          <div className="w-full flex flex-col items-center">
+            <div className="w-full max-w-md">
+              <h2 className="text-xl self-start mb-2">Recipes</h2>
+              <div className="space-y-4">
+                {recipesList.map((recipe, k) => (
+                  <SectionItem
+                    link={"/recipes/" + recipe.slug.toLowerCase()}
+                    name={recipe.attributes.title}
+                    description={recipe.attributes.description}
+                    icon={recipe.attributes.thumbnail}
+                    key={k}
+                    className={"max-w-md w-full"}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
