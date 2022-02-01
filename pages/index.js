f8bd9e1ff4c707760a49bfcd5b5381324716c49c
Bailey Kane <bailey.orion.kane@gmail.com>
2022-02-01 11:44:53 +0800

    Update to use BaseLayout

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from "next/head";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
-import NavBar from "../components/NavBar";
 import SectionItem from "../components/SectionItem";
+import BaseLayout from "../components/BaseLayout";
 
 export default class Home extends Component {
   render() {
     let { title, sections } = attributes;
     return (
-      <div className="flex flex-col h-full min-h-screen w-full min-w-screen bg-gradient-to-b from-zinc-100 to-zinc-200 dark:from-zinc-800 dark:to-zinc-900 dark:text-gray-100 items-center select-none">
+      <>
         <Head>
           <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
-        <div className="max-w-2xl w-full p-2 pb-4 space-y-4">
-          <NavBar className="invisible" />
+        <BaseLayout navbarVisible={false}>
           <div className="w-full flex flex-col items-center">
             <div className="w-full max-w-md space-y-4">
               <div className="text-xl">
@@ -33,8 +32,8 @@ export default class Home extends Component {
               ))}
             </div>
           </div>
-        </div>
-      </div>
+        </BaseLayout>
+      </>
     );
   }
 }
