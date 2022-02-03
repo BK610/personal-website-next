654f92308a88fc3c0f3ea0e55d502d1e8b849d39
Bailey Kane <bailey.orion.kane@gmail.com>
2022-02-04 00:53:59 +0800

    Update formatting. Remove access token from source control.

--- components/BaseMap.js
+++ components/BaseMap.js
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
 import mapboxgl from "mapbox-gl";
-// import "./BaseMap.scss";
+import config from "../config.local.json";
 
 const BaseMap = () => {
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoiYms2MTAiLCJhIjoiY2t6NzF6aXNlMDhsZjMybW10cGU1eHNhaSJ9.CRk8M6h9zT_j4mieCgI9bg";
+  mapboxgl.accessToken = config.MAPBOX_ACCESS_TOKEN;
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: "mapContainer",
-      style: "mapbox://styles/bk610/ckz729y46001t14uvxg7zuqpf",
-      center: [-71.95359716485645, 43.425838945936015],
+      style: "mapbox://styles/bk610/ckz729y46001t14uvxg7zuqpf", // My custom style, from Mapbox Studio configuration
+      center: [-71.95359716485645, 43.425838945936015], // Centered on Pleasant Lake
       zoom: 9,
     });
 
