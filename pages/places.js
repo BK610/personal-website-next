79b514dda3e0a54a987cc6a1fd1aa3de362edc52
Bailey Kane <bailey.orion.kane@gmail.com>
2022-02-04 00:40:09 +0800

    Set up page for basic display of mapbox info

--- pages/places.js
+++ pages/places.js
@@ -1,13 +1,32 @@
+import Head from "next/head";
 import { Component } from "react";
 import BaseLayout from "../components/BaseLayout";
-import MissingContent from "../components/MissingContent";
+import BaseMap from "../components/BaseMap";
 
 export default class Places extends Component {
   render() {
     return (
-      <BaseLayout titleText={"Places"}>
-        <MissingContent />
-      </BaseLayout>
+      <>
+        <Head>
+          <link
+            href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css"
+            rel="stylesheet"
+          />
+        </Head>
+        <BaseLayout titleText={"Places"}>
+          <div className="space-y-4">
+            <h2 className="text-xl">Places</h2>
+            <p className="text-lg">Where I have been...</p>
+            <div
+              className="w-full h-96 overflow-hidden
+          rounded-lg
+          border border-zinc-500"
+            >
+              <BaseMap />
+            </div>
+          </div>
+        </BaseLayout>
+      </>
     );
   }
 }
