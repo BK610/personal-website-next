ed6f88ec84c6c207de61a4d086913603d1712637
Bailey Kane <bailey.orion.kane@gmail.com>
2022-02-12 18:13:54 +0800

    Update color styling

--- components/SectionItem.js
+++ components/SectionItem.js
@@ -10,16 +10,17 @@ this includes props.children.
 
 Specifying each of these in the parent div **is required**, otherwise they don't pass
 to the children correctly, and the links don't work.
+
+hover:from-violet-200 hover:to-orange-200 hover:dark:from-violet-700 hover:dark:to-orange-500
 */
 
 const SectionItem = React.forwardRef(({ onClick, href, ...props }, ref) => {
   return (
     <div
       className={`${props.className} rounded-lg
-               bg-gradient-to-r from-violet-100 to-orange-100 dark:from-violet-600 dark:to-orange-400
-               hover:from-violet-200 hover:to-orange-200 hover:dark:from-violet-700 hover:dark:to-orange-500
-               border-2 border-violet-300 active:border-violet-400 dark:border-zinc-300 active:dark:border-zinc-50
-               shadow-md hover:shadow-lg shadow-violet-300 hover:shadow-violet-300 dark:shadow-violet-800 hover:dark:shadow-violet-800
+               bg-gradient-to-r from-myorange-100 to-mypurple-200 dark:from-mygreen-600 dark:to-mygreen-400
+               border-2 border-myorange-300 active:border-mypurple-300 dark:border-myorange-100 active:dark:border-myorange-50
+               shadow-md hover:shadow-lg
                `}
       ref={ref}
       href={href}
