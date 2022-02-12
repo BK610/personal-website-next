43d3a412e735012353fc1768b0118bd058bfcb24
Bailey Kane <bailey.orion.kane@gmail.com>
2022-02-12 11:58:29 +0800

    Removed use-case-specific styling. To be added via classname props instead

--- components/SectionItem.js
+++ components/SectionItem.js
@@ -17,12 +17,11 @@ const SectionItem = React.forwardRef(({ onClick, href, ...props }, ref) => {
   console.log(props);
   return (
     <div
-      className={`group p-2 rounded-lg cursor-pointer
+      className={`${props.className} rounded-lg
                bg-gradient-to-r from-violet-100 to-orange-100 dark:from-violet-600 dark:to-orange-400
                hover:from-violet-200 hover:to-orange-200 hover:dark:from-violet-700 hover:dark:to-orange-500
                border-2 border-violet-300 active:border-violet-400 dark:border-zinc-300 active:dark:border-zinc-50
                shadow-md hover:shadow-lg shadow-violet-300 hover:shadow-violet-300 dark:shadow-violet-800 hover:dark:shadow-violet-800
-               transition hover:scale-105
                `}
       ref={ref}
       href={href}
