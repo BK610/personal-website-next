51322c40b89c23f1e3ecbc66743a13fa6b36bdcb
Bailey Kane <bailey.orion.kane@gmail.com>
2022-02-12 18:11:10 +0800

    Add custom colors,  and a CSS class for section-list

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,45 @@ module.exports = {
   ],
   theme: {
     extend: {
+      /*
+      Generated on https://palettte.app. Exported JSON at the end of the file.
+      */
+      colors: {
+        mygreen: {
+          900: "#09341E",
+          800: "#134A2D",
+          700: "#20603D",
+          600: "#30764E",
+          500: "#448B62",
+          400: "#5CA178",
+          300: "#77B78C",
+          200: "#95CDAA",
+          100: "#B7E2C6",
+          50: "#DDF8E6",
+        },
+        myorange: {
+          900: "#F28118",
+          800: "#F48B2C",
+          700: "#F59641",
+          600: "#F7A155",
+          500: "#F8AC6A",
+          400: "#F9B87F",
+          300: "#FBC494",
+          200: "#FCD0AA",
+          100: "#FCEAD1",
+          50: "#FDF5E9",
+        },
+        mypurple: {
+          600: "#8761BD",
+          500: "#9A75CE",
+          400: "#AE8CDE",
+          300: "#C3A4EF",
+          200: "#D9BEFF",
+        },
+      },
       backgroundImage: {
-        "base": "url('/img/base.svg')",
-        'base-dark': "url('/img/base-dark.svg')",
+        base: "url('/img/base.svg')",
+        "base-dark": "url('/img/base-dark.svg')",
       },
       keyframes: {
         wiggle: {
@@ -62,3 +98,124 @@ module.exports = {
     require("@tailwindcss/line-clamp"),
   ],
 };
+
+/*
+Generated JSON for color configuration from https://palettte.app
+[
+  {
+    "paletteName": "New Palette",
+    "swatches": [
+      {
+        "name": "New Swatch",
+        "color": "0D4729"
+      },
+      {
+        "name": "New Swatch",
+        "color": "135833"
+      },
+      {
+        "name": "New Swatch",
+        "color": "19693D"
+      },
+      {
+        "name": "New Swatch",
+        "color": "217B48"
+      },
+      {
+        "name": "New Swatch",
+        "color": "2A8C53"
+      },
+      {
+        "name": "New Swatch",
+        "color": "349D5E"
+      },
+      {
+        "name": "New Swatch",
+        "color": "3EAE69"
+      },
+      {
+        "name": "New Swatch",
+        "color": "4AC075"
+      },
+      {
+        "name": "New Swatch",
+        "color": "57D181"
+      },
+      {
+        "name": "New Swatch",
+        "color": "65E28E"
+      }
+    ]
+  },
+  {
+    "paletteName": "New Palette",
+    "swatches": [
+      {
+        "name": "New Swatch",
+        "color": "F28118"
+      },
+      {
+        "name": "New Swatch",
+        "color": "F48B2C"
+      },
+      {
+        "name": "New Swatch",
+        "color": "F59641"
+      },
+      {
+        "name": "New Swatch",
+        "color": "F7A155"
+      },
+      {
+        "name": "New Swatch",
+        "color": "F8AC6A"
+      },
+      {
+        "name": "New Swatch",
+        "color": "F9B87F"
+      },
+      {
+        "name": "New Swatch",
+        "color": "FBC494"
+      },
+      {
+        "name": "New Swatch",
+        "color": "FCD0AA"
+      },
+      {
+        "name": "New Swatch",
+        "color": "FEDCC0"
+      },
+      {
+        "name": "New Swatch",
+        "color": "FFE9D6"
+      }
+    ]
+  },
+  {
+    "paletteName": "New Palette",
+    "swatches": [
+      {
+        "name": "New Swatch",
+        "color": "8761BD"
+      },
+      {
+        "name": "New Swatch",
+        "color": "9A75CE"
+      },
+      {
+        "name": "New Swatch",
+        "color": "AE8CDE"
+      },
+      {
+        "name": "New Swatch",
+        "color": "C3A4EF"
+      },
+      {
+        "name": "New Swatch",
+        "color": "D9BEFF"
+      }
+    ]
+  }
+]
+*/
