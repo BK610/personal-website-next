6ca7f719737d8c45985519812a900beee101b876
Bailey Kane <bailey.orion.kane@gmail.com>
2022-03-05 18:23:09 +0800

    Added WIP comment

--- pages/about.js
+++ pages/about.js
@@ -7,15 +7,14 @@ export default class About extends Component {
       <BaseLayout titleText={"About"}>
         <div className="grid grid-cols-2 gap-y-4">
           <div className="col-span-2 sm:col-span-1">
-            <p>
-              Hello!
-            </p>
+            Hello! Work in progress.
           </div>
           <div className="col-span-2 sm:col-span-1 place-self-center sm:justify-self-auto">
             <img
               className="rounded-lg border border-stone-800 dark:border-stone-200 object-cover h-72 w-72 sm:w-full"
               src={`/img/Headshot.jpg`}
             />
+            <p className="mt-1 text-center">☝️ Also a work in progress.</p>
           </div>
         </div>
       </BaseLayout>
