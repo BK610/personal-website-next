d58c991c1c91a859b6b4452cdcd085223be36c5f
BK610 <bailey.orion.kane@gmail.com>
2024-12-28 12:30:30 -0500

    Remove extra logging

--- components/SectionItemHeading.js
+++ components/SectionItemHeading.js
@@ -1,7 +1,4 @@
 function SectionItemHeading(props) {
-  console.log("Props:")
-  console.log(props)
-  console.log(props.className)
   return (
     <div className={`${props.className} text-lg font-serif`}>
       <h2>{props.children}</h2>
