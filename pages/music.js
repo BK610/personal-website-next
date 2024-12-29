cd141a4d798640229bec039642a530edbbab5435
BK610 <bailey.orion.kane@gmail.com>
2024-12-29 17:00:13 -0500

    Rework to use getStaticProps to move data fetching to server-side. Big performance improvements

--- pages/music.js
+++ pages/music.js
@@ -1,39 +1,41 @@
-import { Component } from "react";
 import BaseLayout from "../components/BaseLayout";
 import Head from "next/head";
 import MusicItem from "../components/MusicItem";
 import PageHeading from "../components/PageHeading";
 import { importCSVDataAsJson } from "../lib/sheetsConnector";
 
-export default class Music extends Component {
-  static async getInitialProps() {
-    const musicList = await importCSVDataAsJson(
-      process.env.NEXT_PUBLIC_MUSIC_DATA_URL
-    );
-    return { musicList };
-  }
-
-  render() {
-    const { musicList } = this.props;
-    return (
-      <>
-        <Head>
-          {/* Makes all links on the page open in a new tab.
+export default function Music({ musicList }) {
+  return (
+    <>
+      <Head>
+        {/* Makes all links on the page open in a new tab.
           From here: https://stackoverflow.com/a/5788445 */}
-          <base target="_blank" />{" "}
-        </Head>
-        <BaseLayout titleText={"Music"}>
-          <div className="w-full flex flex-col items-center">
-            <div className="w-full max-w-md space-y-4">
-              <PageHeading>Music</PageHeading>
-              <div className="text-lg">Small moments of making music</div>
-              {musicList.data.map((musicItem, k) => (
-                <MusicItem info={musicItem} key={k} />
-              ))}
-            </div>
+        <base target="_blank" />{" "}
+      </Head>
+      <BaseLayout titleText={"Music"}>
+        <div className="w-full flex flex-col items-center">
+          <div className="w-full max-w-md space-y-4">
+            <PageHeading>Music</PageHeading>
+            <div className="text-lg">Small moments of making music</div>
+            {musicList.data.map((musicItem, k) => (
+              <MusicItem info={musicItem} key={k} />
+            ))}
           </div>
-        </BaseLayout>
-      </>
-    );
-  }
+        </div>
+      </BaseLayout>
+    </>
+  );
+}
+
+// Reference: https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-props#using-getstaticprops-to-fetch-data-from-a-cms
+export async function getStaticProps() {
+  const musicList = await importCSVDataAsJson(
+    process.env.NEXT_PUBLIC_MUSIC_DATA_URL
+  );
+
+  return {
+    props: {
+      musicList,
+    },
+  };
 }
--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
 import RecipeSectionItem from "../../components/RecipeSectionItem";
 import BaseLayout from "../../components/BaseLayout";
 import SectionList from "../../components/SectionList";
 import PageHeading from "../../components/PageHeading";
 import { importCSVDataAsJson } from "../../lib/sheetsConnector";
 
-export default class Recipes extends Component {
-  static async getInitialProps() {
-    const recipesList = await importCSVDataAsJson(
-      process.env.NEXT_PUBLIC_RECIPES_DATA_URL
-    );
-
-    return { recipesList };
-  }
-
-  render() {
-    const { recipesList } = this.props;
-    return (
-      <BaseLayout navbarVisible={true} titleText={"Recipes"}>
-        <div className="w-full flex flex-col items-center">
-          <div className="w-full max-w-md space-y-4">
-            <PageHeading>Recipes</PageHeading>
-            <div className="text-lg">Behold, my lovely recipes.</div>
-            <SectionList>
-              {recipesList.data.map((recipe, k) => (
-                <RecipeSectionItem
-                  link={"/recipes/" + recipe.slug}
-                  name={recipe.title}
-                  description={recipe.description}
-                  icon={recipe.thumbnail}
-                  key={k}
-                  className={"max-w-md w-full"}
-                />
-              ))}
-            </SectionList>
-          </div>
+export default function Recipes({ recipesList }) {
+  return (
+    <BaseLayout navbarVisible={true} titleText={"Recipes"}>
+      <div className="w-full flex flex-col items-center">
+        <div className="w-full max-w-md space-y-4">
+          <PageHeading>Recipes</PageHeading>
+          <div className="text-lg">Behold, my lovely recipes.</div>
+          <SectionList>
+            {recipesList.data.map((recipe, k) => (
+              <RecipeSectionItem
+                link={"/recipes/" + recipe.slug}
+                name={recipe.title}
+                description={recipe.description}
+                icon={recipe.thumbnail}
+                key={k}
+                className={"max-w-md w-full"}
+              />
+            ))}
+          </SectionList>
         </div>
-      </BaseLayout>
-    );
-  }
+      </div>
+    </BaseLayout>
+  );
+}
+
+// Reference: https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-props#using-getstaticprops-to-fetch-data-from-a-cms
+export async function getStaticProps() {
+  const recipesList = await importCSVDataAsJson(
+    process.env.NEXT_PUBLIC_RECIPES_DATA_URL
+  );
+
+  return {
+    props: { recipesList },
+  };
 }
