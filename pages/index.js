adb3842522b4be6395218ba1fc64f13235435a35
BK610 <bailey.orion.kane@gmail.com>
2024-12-29 15:33:57 -0500

    Remove Netlify Identity script for CMS management

--- pages/index.js
+++ pages/index.js
@@ -20,43 +20,35 @@ export default class Home extends Component {
   render() {
     const { sectionsList } = this.props;
     return (
-      <>
-        <Head>
-          <script
-            defer
-            src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-          ></script>
-        </Head>
-        <BaseLayout navbarVisible={false} className="">
-          <div className="w-full flex flex-col relative -mt-4">
-            <div className="w-full flex flex-col items-center absolute z-10 mt-4">
-              <div className="h-full w-full max-w-md space-y-4">
-                <PageHeading>Hello hello, this is Bailey speaking.</PageHeading>
-                <div className="prose prose-stone dark:prose-invert">
-                  Welcome to the home of my many interests, half-baked projects,
-                  and digital representations of myself.
-                  <br />
-                  <br />
-                  Hope you enjoy.
-                </div>
-                <SectionList>
-                  {sectionsList.data.map((section, k) => (
-                    <HomeSectionItem
-                      link={
-                        section.link ? section.link : section.name.toLowerCase()
-                      }
-                      name={section.name}
-                      description={section.description}
-                      icon={section.icon}
-                      key={k}
-                    />
-                  ))}
-                </SectionList>
+      <BaseLayout navbarVisible={false} className="">
+        <div className="w-full flex flex-col relative -mt-4">
+          <div className="w-full flex flex-col items-center absolute z-10 mt-4">
+            <div className="h-full w-full max-w-md space-y-4">
+              <PageHeading>Hello hello, this is Bailey speaking.</PageHeading>
+              <div className="prose prose-stone dark:prose-invert">
+                Welcome to the home of my many interests, half-baked projects,
+                and digital representations of myself.
+                <br />
+                <br />
+                Hope you enjoy.
               </div>
+              <SectionList>
+                {sectionsList.data.map((section, k) => (
+                  <HomeSectionItem
+                    link={
+                      section.link ? section.link : section.name.toLowerCase()
+                    }
+                    name={section.name}
+                    description={section.description}
+                    icon={section.icon}
+                    key={k}
+                  />
+                ))}
+              </SectionList>
             </div>
           </div>
-        </BaseLayout>
-      </>
+        </div>
+      </BaseLayout>
     );
   }
 }
