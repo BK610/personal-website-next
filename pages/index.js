204ecffaa0ed8df216885b025e0bd8709d61d88c
BK610 <bailey.orion.kane@gmail.com>
2024-12-30 10:59:56 -0500

    Remove redundant flexbox setup. No idea where that came from

--- pages/index.js
+++ pages/index.js
@@ -7,31 +7,27 @@ import { importCSVDataAsJson } from "../lib/sheetsConnector";
 export default function Home({ sectionsList }) {
   return (
     <BaseLayout navbarVisible={false} className="">
-      <div className="w-full flex flex-col relative -mt-4">
-        <div className="w-full flex flex-col items-center absolute z-10 mt-4">
-          <div className="h-full w-full max-w-md space-y-4">
-            <PageHeading>Hello hello, this is Bailey speaking.</PageHeading>
-            <div className="prose prose-stone dark:prose-invert">
-              Welcome to the home of my many interests, half-baked projects, and
-              digital representations of myself.
-              <br />
-              <br />
-              Hope you enjoy.
-            </div>
-            <SectionList>
-              {sectionsList.data.map((section, k) => (
-                <HomeSectionItem
-                  link={
-                    section.link ? section.link : section.name.toLowerCase()
-                  }
-                  name={section.name}
-                  description={section.description}
-                  icon={section.icon}
-                  key={k}
-                />
-              ))}
-            </SectionList>
+      <div className="w-full flex flex-col items-center relative z-10">
+        <div className="h-full w-full max-w-md space-y-4">
+          <PageHeading>Hello hello, this is Bailey speaking.</PageHeading>
+          <div className="prose prose-stone dark:prose-invert">
+            Welcome to the home of my many interests, half-baked projects, and
+            digital representations of myself.
+            <br />
+            <br />
+            Hope you enjoy.
           </div>
+          <SectionList>
+            {sectionsList.data.map((section, k) => (
+              <HomeSectionItem
+                link={section.link ? section.link : section.name.toLowerCase()}
+                name={section.name}
+                description={section.description}
+                icon={section.icon}
+                key={k}
+              />
+            ))}
+          </SectionList>
         </div>
       </div>
     </BaseLayout>
