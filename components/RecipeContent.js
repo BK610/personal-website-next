9d851ca44258c8ecc980d335654bf467959e9a56
BK610 <bailey.orion.kane@gmail.com>
2025-01-05 11:40:29 -0500

    Move recipe content into a separate component

---
+++ components/RecipeContent.js
@@ -0,0 +1,65 @@
+import { micromark } from "micromark";
+
+export default function RecipeContent({ recipe }) {
+  const {
+    title,
+    date,
+    thumbnail,
+    description,
+    author,
+    prepTime,
+    totalTime,
+    ingredients,
+    content,
+  } = recipe;
+
+  const dateObj = new Date(date);
+  return (
+    <div className="prose prose-stone dark:prose-invert mt-4">
+      <h1>{title}</h1>
+      <div className="border-b border-b-stone-500">
+        <span className="no-underline">{description}</span>
+      </div>
+      <div className="grid grid-cols-2">
+        <div className="col-span-2 sm:col-span-1 place-self-center">
+          <img
+            className="rounded-lg object-contain max-h-72"
+            src={`../${thumbnail}`}
+          />
+        </div>
+        <div className="col-span-2 sm:col-span-1 grid grid-cols-2 sm:grid-cols-3 space-x-2 self-center">
+          <div className="justify-self-end text-right underline decoration-1">
+            <div>Author</div>
+            <div>Prep Time</div>
+            <div>Total Time</div>
+            <div>Published</div>
+          </div>
+          <div className="col-span-1 sm:col-span-2 justify-end">
+            <div>{author}</div>
+            <div>{prepTime} min</div>
+            <div>{totalTime} min</div>
+            <div>
+              {dateObj.toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "short",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Ingredients</h2>
+        <div
+          className="select-all"
+          dangerouslySetInnerHTML={{ __html: micromark(ingredients) }}
+        />
+      </div>
+      <div>
+        <h2>Recipe</h2>
+        <div dangerouslySetInnerHTML={{ __html: micromark(content) }} />
+      </div>
+    </div>
+  );
+}
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
 import MissingContent from "../../components/MissingContent";
-import { micromark } from "micromark";
 import BaseLayout from "../../components/BaseLayout";
+import RecipeContent from "../../components/RecipeContent";
 import { importCSVDataAsJson } from "../../lib/sheetsConnector";
 
 export default function Recipe({ recipe }) {
@@ -9,68 +8,9 @@ export default function Recipe({ recipe }) {
     return <MissingContent />;
   }
 
-  const {
-    title,
-    date,
-    thumbnail,
-    description,
-    author,
-    prepTime,
-    totalTime,
-    ingredients,
-    content,
-  } = recipe;
-
-  const dateObj = new Date(date);
-
   return (
     <BaseLayout>
-      <div className="prose prose-stone dark:prose-invert mt-4">
-        <h1>{title}</h1>
-        <div className="border-b border-b-stone-500">
-          <span className="no-underline">{description}</span>
-        </div>
-        <div className="grid grid-cols-2">
-          <div className="col-span-2 sm:col-span-1 place-self-center">
-            <img
-              className="rounded-lg object-contain max-h-72"
-              src={`../${thumbnail}`}
-            />
-          </div>
-          <div className="col-span-2 sm:col-span-1 grid grid-cols-2 sm:grid-cols-3 space-x-2 self-center">
-            <div className="justify-self-end text-right underline decoration-1">
-              <div>Author</div>
-              <div>Prep Time</div>
-              <div>Total Time</div>
-              <div>Published</div>
-            </div>
-            <div className="col-span-1 sm:col-span-2 justify-end">
-              <div>{author}</div>
-              <div>{prepTime} min</div>
-              <div>{totalTime} min</div>
-              <div>
-                {dateObj.toLocaleDateString("en-US", {
-                  weekday: "long",
-                  month: "short",
-                  day: "numeric",
-                  year: "numeric",
-                })}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div>
-          <h2>Ingredients</h2>
-          <div
-            className="select-all"
-            dangerouslySetInnerHTML={{ __html: micromark(ingredients) }}
-          />
-        </div>
-        <div>
-          <h2>Recipe</h2>
-          <div dangerouslySetInnerHTML={{ __html: micromark(content) }} />
-        </div>
-      </div>
+      <RecipeContent recipe={recipe} />
     </BaseLayout>
   );
 }
