f0d415bf80b32606f9dacfc3261b0e20278718cc
BK610 <bailey.orion.kane@gmail.com>
2025-01-05 12:53:35 -0500

    Remove unnecessary div

--- components/PageHeading.js
+++ components/PageHeading.js
@@ -1,7 +1,3 @@
 export default function PageHeading({ children }) {
-  return (
-    <div className="text-2xl font-normal font-serif">
-      <h2>{children}</h2>
-    </div>
-  );
+  return <h2 className="text-2xl font-normal font-serif">{children}</h2>;
 }
