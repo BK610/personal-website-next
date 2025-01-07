523ff8224c55c9f79471a59cd3f8fd655f439e42
BK610 <bailey.orion.kane@gmail.com>
2025-01-06 22:56:12 -0500

    Add getBlueskyUri function to convert the AT Protocol URI to a Bluesky URI. Make each post a link to that post on my website. Update styling.

--- components/BlueskyFeed/BlueskyPost.js
+++ components/BlueskyFeed/BlueskyPost.js
@@ -2,19 +2,30 @@ import BlueskyPostHeader from "./BlueskyPostHeader";
 import BlueskyPostText from "./BlueskyPostText";
 
 export default function BlueskyPost({ post_data }) {
-  const { author, record } = post_data.post;
-
-  // console.log(post_data);
+  const { author, record, uri } = post_data.post;
+  const blueskyUri = getBlueskyUri(author.handle, uri);
 
   return (
-    <a
-      // href=""
-      target="_blank"
-    >
-      <div className="border border-stone-800 dark:border-stone-200 rounded-lg p-4 my-4">
+    <a href={blueskyUri} target="_blank">
+      <div
+        className="p-4 my-4 bg-stone-200 dark:bg-stone-800 rounded-lg
+      border border-transparent border-b-stone-900 dark:border-b-stone-200 hover:border-stone-900 hover:dark:border-stone-200 "
+      >
         <BlueskyPostHeader author={author} record={record} />
         <BlueskyPostText text={record.text} />
       </div>
     </a>
   );
+}
+
+function getBlueskyUri(handle, atProtoUri) {
+  var blueskyUri = "https://bsky.app/profile/<handle>/post/<postId>";
+
+  const postId = atProtoUri.substring(atProtoUri.lastIndexOf("/") + 1);
+
+  blueskyUri = blueskyUri
+    .replace("<handle>", handle)
+    .replace("<postId>", postId);
+
+  return blueskyUri;
 }
