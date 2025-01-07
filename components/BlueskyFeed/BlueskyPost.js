9162f9aff4cb4c542954d25454710288966a65de
BK610 <bailey.orion.kane@gmail.com>
2025-01-06 23:55:08 -0500

    Update styling. Put the avatar on the left, improve the default and hover state border styling, minor other changes

--- components/BlueskyFeed/BlueskyPost.js
+++ components/BlueskyFeed/BlueskyPost.js
@@ -9,10 +9,20 @@ export default function BlueskyPost({ post_data }) {
     <a href={blueskyUri} target="_blank">
       <div
         className="p-4 my-4 bg-stone-200 dark:bg-stone-800 rounded-lg
-      border border-transparent border-b-stone-900 dark:border-b-stone-200 hover:border-stone-900 hover:dark:border-stone-200 "
+      border border-stone-300 dark:border-stone-500 border-b-stone-900 dark:border-b-stone-200 hover:border-stone-900 hover:dark:border-stone-200 "
       >
-        <BlueskyPostHeader author={author} record={record} />
-        <BlueskyPostText text={record.text} />
+        <div className="flex w-full items-start">
+          <img
+            className="mr-4 border border-stone-900 dark:border-stone-500 rounded-full"
+            src={author.avatar}
+            height={"50px"}
+            width={"50px"}
+          />
+          <div className="w-full">
+            <BlueskyPostHeader author={author} record={record} />
+            <BlueskyPostText text={record.text} />
+          </div>
+        </div>
       </div>
     </a>
   );
