e75cb8520f5b23dc06482574d04126dd81e6850a
BK610 <bailey.orion.kane@gmail.com>
2025-01-08 14:26:03 -0500

    Rough draft of adding 'reposted by' text

--- components/BlueskyFeed/BlueskyPost.js
+++ components/BlueskyFeed/BlueskyPost.js
@@ -3,7 +3,11 @@ import BlueskyPostText from "./BlueskyPostText";
 
 export default function BlueskyPost({ post_data }) {
   const { author, record, uri } = post_data.post;
+  const { reason } = post_data;
   const blueskyUri = getBlueskyUri(author.handle, uri);
+  const isRepost =
+    typeof reason !== "undefined" &&
+    reason.$type === "app.bsky.feed.defs#reasonRepost";
 
   return (
     <a href={blueskyUri} target="_blank">
@@ -11,16 +15,23 @@ export default function BlueskyPost({ post_data }) {
         className="p-4 my-4 bg-stone-200 dark:bg-stone-800 rounded-lg
       border border-stone-300 dark:border-stone-500 border-b-stone-900 dark:border-b-stone-200 hover:border-stone-900 hover:dark:border-stone-200 "
       >
-        <div className="flex w-full items-start">
-          <img
-            className="mr-4 border border-stone-900 dark:border-stone-500 rounded-full"
-            src={author.avatar}
-            height={"50px"}
-            width={"50px"}
-          />
-          <div className="w-full">
-            <BlueskyPostHeader author={author} record={record} />
-            <BlueskyPostText text={record.text} />
+        <div className="flex flex-col w-full">
+          {isRepost && (
+            <div className="ml-16 -mt-2 pb-2 text-xs text-stone-700 dark:text-stone-400">
+              🔁 Reposted by {reason.by.displayName}
+            </div>
+          )}
+          <div className="flex w-full items-start">
+            <img
+              className="mr-4 border border-stone-900 dark:border-stone-500 rounded-full"
+              src={author.avatar}
+              height={"50px"}
+              width={"50px"}
+            />
+            <div className="w-full">
+              <BlueskyPostHeader author={author} record={record} />
+              <BlueskyPostText text={record.text} />
+            </div>
           </div>
         </div>
       </div>
