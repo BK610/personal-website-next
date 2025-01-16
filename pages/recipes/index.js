062b830a851967118f4936e185480d6eb15128fc
BK610 <bailey.orion.kane@gmail.com>
2025-01-15 21:28:02 -0500

    Make max styling wider, add spacing between items

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -7,10 +7,10 @@ export default function Recipes({ recipesList }) {
   return (
     <BaseLayout navbarVisible={true} titleText={"Recipes"}>
       <div className="w-full flex flex-col items-center">
-        <div className="w-full max-w-md space-y-4">
+        <div className="w-full max-w-lg space-y-4">
           <h2>Recipes</h2>
           <div className="text-lg">Behold, my lovely recipes.</div>
-          <SectionList>
+          <SectionList className="space-y-4">
             {recipesList.data.map((recipe, k) => (
               <RecipeSectionItem
                 link={"/recipes/" + recipe.slug}
