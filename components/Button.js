42ca90a0750721010c6224e077ad13aa54abc1d1
BK610 <bailey.orion.kane@gmail.com>
2025-01-20 21:42:20 -0500

    Create Button component

---
+++ components/Button.js
@@ -0,0 +1,13 @@
+export default function Button({ href, className }) {
+  return (
+    <a href={href} target="_blank">
+      <button
+        className={`px-4 py-2 font-bold
+          bg-gradient-to-r from-purple-200 to-orange-100 dark:from-purple-500 dark:to-orange-300
+          border border-stone-800 dark:border-stone-200 rounded-full hover:opacity-80 active:opacity-60 ${className}`}
+      >
+        Visit →
+      </button>
+    </a>
+  );
+}
