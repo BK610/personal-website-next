8f70e75722292041f950288e788024c9d8ddff4f
BK610 <bailey.orion.kane@gmail.com>
2025-01-22 11:13:02 -0500

    Update to use suggested Pages Router structure for PostHog setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,45 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
+import { Router } from "next/router";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== "undefined") {
-  // checks that we are client-side
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
-    },
-  });
-}
+// if (typeof window !== "undefined") {
+//   // checks that we are client-side
+//   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+//     api_host:
+//       process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+//     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+//     loaded: (posthog) => {
+//       if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+//     },
+//   });
+// }
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      // person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+      },
+    });
+
+    const handleRouteChange = () => posthog?.capture("$pageview");
+
+    Router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <>
-      <PostHogProvider client={posthog}>
-        <Component {...pageProps} />
-      </PostHogProvider>
-    </>
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
   );
 }
 
