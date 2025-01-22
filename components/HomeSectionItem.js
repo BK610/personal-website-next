7f57eeddd055cd0d9092300b7ead498ead5914a6
BK610 <bailey.orion.kane@gmail.com>
2025-01-22 11:44:37 -0500

    Remove extra <a> tags inside next/Link element

--- components/HomeSectionItem.js
+++ components/HomeSectionItem.js
@@ -6,18 +6,16 @@ export default class HomeSectionItem extends Component {
   render() {
     return (
       <Link href={this.props.link} passHref>
-        <SectionItem className="group transition hover:scale-105 p-2 cursor-pointer">
-          <a className="grid grid-cols-4" href={this.props.link}>
-            <div className="self-center col-span-3">
-              <h3 className="mb-1">{this.props.name}</h3>
-              <p className="text-stone-700 font-light dark:text-stone-100">
-                {this.props.description}
-              </p>
-            </div>
-            <div className="text-3xl justify-self-end self-center pr-3 group-hover:animate-spin">
-              {this.props.emoji}
-            </div>
-          </a>
+        <SectionItem className="group transition hover:scale-105 p-2 cursor-pointer grid grid-cols-4">
+          <div className="self-center col-span-3">
+            <h3 className="mb-1">{this.props.name}</h3>
+            <p className="text-stone-700 font-light dark:text-stone-100">
+              {this.props.description}
+            </p>
+          </div>
+          <div className="text-3xl justify-self-end self-center pr-3 group-hover:animate-spin">
+            {this.props.emoji}
+          </div>
         </SectionItem>
       </Link>
     );
--- components/RecipeSectionItem.js
+++ components/RecipeSectionItem.js
@@ -6,27 +6,22 @@ export default class RecipeSectionItem extends Component {
   render() {
     return (
       <Link href={this.props.link} passHref>
-        <SectionItem className="h-32 grid grid-rows-1 group cursor-pointer transition hover:scale-105">
-          <a
-            className="h-full w-full grid grid-cols-3 self-center items-center"
-            href={this.props.link}
-          >
-            <div className="p-2 col-span-2">
-              <h3 className="mb-1">{this.props.name}</h3>
-              <p
-                className="font-light text-stone-700 dark:text-stone-100
+        <SectionItem className="h-32 group cursor-pointer transition hover:scale-105 w-full grid grid-cols-3 self-center items-center">
+          <div className="p-2 col-span-2">
+            <h3 className="mb-1">{this.props.name}</h3>
+            <p
+              className="font-light text-stone-700 dark:text-stone-100
               line-clamp-3"
-              >
-                {this.props.description}
-              </p>
-            </div>
-            <div className="h-full w-full overflow-hidden">
-              <img
-                className="self-center h-full w-full object-cover"
-                src={this.props.icon}
-              ></img>
-            </div>
-          </a>
+            >
+              {this.props.description}
+            </p>
+          </div>
+          <div className="h-full w-full overflow-hidden">
+            <img
+              className="self-center h-full w-full object-cover"
+              src={this.props.icon}
+            ></img>
+          </div>
         </SectionItem>
       </Link>
     );
