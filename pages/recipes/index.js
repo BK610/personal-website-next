e8cc8671e4a25e6c65945c5121109e347fa30588
BK610 <bailey.orion.kane@gmail.com>
2025-01-30 11:54:05 -0500

    Widen page to make room for multiple columns of recipes

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -7,10 +7,10 @@ export default function Recipes({ recipesList }) {
   return (
     <BaseLayout titleText={"Recipes"}>
       <div className="w-full flex flex-col items-center">
-        <div className="w-full max-w-lg space-y-4">
+        <div className="w-full max-w-5xl space-y-4">
           <h2>Recipes</h2>
           <div className="text-lg">Behold, my lovely recipes.</div>
-          <SectionList className="item-list flex flex-col">
+          <SectionList className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {recipesList.data.map((recipe, k) => (
               <RecipeSectionItem
                 link={"/recipes/" + recipe.slug}
