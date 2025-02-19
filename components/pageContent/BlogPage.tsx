de7c4fb4af7c4d0fcd35b398cc001a730d1081c2
BK610 <bailey.orion.kane@gmail.com>
2025-02-19 15:37:35 -0500

    Minor spacing adjustments

--- components/pageContent/BlogPage.tsx
+++ components/pageContent/BlogPage.tsx
@@ -1,5 +1,6 @@
 import { Key } from "react";
 import BaseLayout from "@/components/BaseLayout";
+import SectionList from "@/components/SectionList";
 import BlogPostPreview from "@/components/BlogPostPreview";
 import type BlogPost from "@/types/BlogPost";
 
@@ -11,17 +12,17 @@ export default function Blog({ blogPosts }: BlogProps): React.ReactElement {
   return (
     <BaseLayout titleText="Blog">
       <div className="max-w-2xl mx-auto">
-        <div>
+        <div className="mb-6">
           <h1>Blog</h1>
           <p className="pt-2">
             Collected thoughts, very much a work in progress.
           </p>
         </div>
-        <div>
+        <SectionList>
           {blogPosts.map((blogPost, k: Key) => (
             <BlogPostPreview key={k} blogPost={blogPost} />
           ))}
-        </div>
+        </SectionList>
       </div>
     </BaseLayout>
   );
