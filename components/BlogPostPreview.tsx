bef97ff2f365cce2749a20577a7dcb0577a546a5
BK610 <bailey.orion.kane@gmail.com>
2025-02-19 15:45:59 -0500

    Basic display of content

--- components/BlogPostPreview.tsx
+++ components/BlogPostPreview.tsx
@@ -1,4 +1,5 @@
 import BlogPost from "@/types/BlogPost";
+import Link from "next/link";
 import React from "react";
 
 interface BlogPostPreviewProps {
@@ -9,8 +10,20 @@ export default function BlogPostPreview({
   blogPost,
 }: BlogPostPreviewProps): React.ReactElement {
   return (
-    <div>
-      <h2>{blogPost.title}</h2>
-    </div>
+    <Link href={blogPost.slug}>
+      <div
+        className={`section-item border-stone-800 dark:border-stone-200 group transition hover:scale-105 p-2 cursor-pointer`}
+      >
+        <div className="">
+          <p className="text-sm mb-2 text-stone-700 dark:text-stone-100">
+            From {blogPost.publishedDate}
+          </p>
+          <h2 className="mb-1 font-bold">{blogPost.title}</h2>
+          <p className="text-stone-700 dark:text-stone-100 line-clamp-3">
+            {blogPost.content}
+          </p>
+        </div>
+      </div>
+    </Link>
   );
 }
