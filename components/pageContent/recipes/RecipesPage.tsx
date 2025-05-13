5483924ed0f1d5e2be0e7e0a175ca7d3f0017aa8
Bailey Kane <bailey.orion.kane@gmail.com>
2025-05-12 20:55:04 -0400

    Add a link to my food bluesky account to the header

--- components/pageContent/recipes/RecipesPage.tsx
+++ components/pageContent/recipes/RecipesPage.tsx
@@ -18,6 +18,17 @@ export default function Recipes({
       <div className="w-full flex flex-col items-center">
         <div className="w-full max-w-5xl space-y-4">
           <h2>Recipes</h2>
+          <div className="text-sm text-stone-700 dark:text-stone-300">
+            See my latest food adventures at{" "}
+            <a
+              href="https://bsky.app/profile/food.baileykane.co"
+              target="_blank"
+              className="underline hover:decoration-2"
+            >
+              @food.baileykane.co
+            </a>{" "}
+            on Bluesky.
+          </div>
           <div className="text-lg">Behold, my lovely recipes.</div>
           <SectionList className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
             {recipesList.data.map((recipe: RecipeType, k: Key) => (
