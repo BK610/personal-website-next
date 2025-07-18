065f86fb4fd0ccdc86aa4fde04596b3476e6eebc
Bailey Kane <bailey.orion.kane@gmail.com>
2025-07-17 23:59:24 -0400

    Add Open Library Covers API to allowed remote image sources

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "covers.openlibrary.org",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
