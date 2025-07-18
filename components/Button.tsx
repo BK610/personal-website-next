3e3474a9a8ef7ddf57b410c4f2329954f8c39796
Bailey Kane <bailey.orion.kane@gmail.com>
2025-07-17 22:47:13 -0400

    Add cursor-pointer to all Buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({
   return (
     <a href={href} target={target}>
       <button
-        className={`px-4 py-2 font-bold
+        className={`px-4 py-2 font-bold cursor-pointer
           bg-linear-to-r from-purple-200 to-orange-100 dark:from-purple-500 dark:to-orange-300
           border border-stone-800 dark:border-stone-200 rounded-full hover:opacity-80 active:opacity-60 ${className}`}
       >
