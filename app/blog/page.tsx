d2cb3ee15bd588fd69ef1073403aa706adc01de4
Bailey Kane <bailey.orion.kane@gmail.com>
2025-07-23 19:47:55 -0400

    Add handler to fetch all published Leaflet documents linked to baileykane.co

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,4 @@
+import { agent } from "@/lib/bskyApi";
 import BlogPage from "@/components/pageContent/blog/BlogPage";
 import { importCSVDataAsJson } from "@/lib/sheetsConnector";
 import type BlogPostType from "@/types/BlogPost";
@@ -6,6 +7,7 @@ export const revalidate = 60;
 
 export default async function Page(): Promise<React.ReactElement> {
   const blogPosts = await getBlogPosts();
+  const leafletPosts = await getLeafletPosts();
 
   return <BlogPage blogPosts={blogPosts} />;
 }
@@ -16,4 +18,12 @@ async function getBlogPosts(): Promise<Array<BlogPostType>> {
   );
 
   return blogPosts.data;
+}
+
+async function getLeafletPosts(): Promise<Array<Object>> {
+  const leafletPosts = await fetch(
+    "https://bsky.social/xrpc/com.atproto.repo.listRecords?repo=did:plc:4zwo7e6xbmv3clz7bj5wsxxu&collection=pub.leaflet.document"
+  ).then((res) => res.json());
+
+  return JSON.parse(JSON.stringify(leafletPosts.records));
 }
