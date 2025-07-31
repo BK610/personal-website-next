c9e0ccbcc03288b1ddcc17fd7f52fd54b33eab41
Bailey Kane <bailey.orion.kane@gmail.com>
2025-07-30 21:07:25 -0400

    Refactor to use functions in the separate richtext file.

--- components/pageContent/feed/BlueskyFeed/BlueskyPostText.js
+++ components/pageContent/feed/BlueskyFeed/BlueskyPostText.js
@@ -1,6 +1,4 @@
-import { RichText } from "@atproto/api";
-import { agent } from "@/lib/bskyApi";
-import { micromark } from "micromark";
+import { parseBlueskyPostText } from "@/lib/richText";
 
 export default function BlueskyPostText({ text }) {
   const parsedText = parseBlueskyPostText(text);
@@ -11,33 +9,4 @@ export default function BlueskyPostText({ text }) {
       dangerouslySetInnerHTML={{ __html: parsedText }}
     ></div>
   );
-}
-
-function parseBlueskyPostText(text) {
-  const rt = new RichText({ text: text });
-
-  rt.detectFacets(agent);
-
-  const markdown = renderAsMarkdown(rt);
-
-  const parsedMarkdown = micromark(markdown);
-
-  return parsedMarkdown;
-}
-
-function renderAsMarkdown(richText) {
-  let markdown = "";
-  for (const segment of richText.segments()) {
-    if (segment.isLink()) {
-      markdown += `[${segment.text}](${segment.link?.uri})`;
-    } else if (segment.isMention()) {
-      markdown += `[${segment.text}](https://bsky.app/profile/${segment.mention?.did})`;
-    } else if (segment.isTag()) {
-      markdown += `[${segment.text}](https://bsky.app/hashtag/${segment.tag?.tag})`;
-    } else {
-      markdown += segment.text;
-    }
-  }
-
-  return markdown;
 }
