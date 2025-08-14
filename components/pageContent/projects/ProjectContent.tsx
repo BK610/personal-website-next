8f926456818234205f98571d76cb7dd387cb37cc
Bailey Kane <bailey.orion.kane@gmail.com>
2025-08-13 20:31:48 -0400

    Add pointer styling to GitHub code buttons

--- components/pageContent/projects/ProjectContent.tsx
+++ components/pageContent/projects/ProjectContent.tsx
@@ -35,7 +35,7 @@ export default function ProjectContent({ project }): React.ReactElement {
           {githubLink && (
             <a href={githubLink} target="_blank">
               <button
-                className="px-4 py-2
+                className="px-4 py-2 cursor-pointer
           bg-linear-to-r from-stone-100 to-stone-200 dark:from-stone-900 dark:to-stone-800
           border border-stone-800 dark:border-stone-200 rounded-full hover:opacity-70"
               >
